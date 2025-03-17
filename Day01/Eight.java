c05de8fc611489692257be8d8842680ff2c88e00
5yndr0m <sdilanjana21@gmail.com>
2025-03-18 04:45:10 +0530

    feat: add product class

---
+++ Day01/Eight.java
@@ -0,0 +1,67 @@
+
+class Product {
+    private int productId;
+    private String name;
+    private double price;
+    private int quantity;
+    
+    public Product(int productId, String name, double price, int quantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    public int getProductId() {
+        return productId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public boolean setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+            return true;
+        }
+        return false;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public boolean setQuantity(int quantity) {
+        if(quantity >= 0) {
+            this.quantity = quantity;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean addStock(int amount) {
+        if (amount > 0) {
+            quantity += amount;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean removeStock(int amount) {
+        if (0 < amount && amount <= quantity) {
+            quantity -= amount;
+            return true;
+        }
+        return false;
+    }
+    
+    public String displayInfo() {
+        return String.format("ID : %d | Name : %s | Price : $%.2f | Quantity : %d", productId, name, price, quantity);
+    }
+}
+
