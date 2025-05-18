693d66f5f1be9e904da13fc962e0ea6133cf9f68
5yndr0m <sdilanjana21@gmail.com>
2025-05-18 22:09:13 +0530

    feat: add methods to perform crud operations

--- AppwithDB/src/main/java/com/vau/app/service/DepartmentService.java
+++ AppwithDB/src/main/java/com/vau/app/service/DepartmentService.java
@@ -21,5 +21,31 @@ public class DepartmentService {
 	public Department getDept(String dep_id) {
 		return repo.findById(dep_id).get();
 	}
+	
+	public String addDept(Department dept) {
+		if(repo.findById(dept.getDepID()).isEmpty()) {
+			repo.save(dept);
+			return "New Department Added";
+		}
+		
+		return "Department with ID: " + dept.getDepID() + " exists";
+	}
+	
+	public String updateDept(String id, Department dept) {
+		if(repo.existsById(id)) {
+			repo.save(dept);
+			return "Department Updated";
+		}
+		return "Department with ID: " + id + "does not exists"; 
+	}
+	
+	public String deleteDept(String id) {
+		if(repo.existsById(id)) {
+			repo.deleteById(id);
+			return "Department Removed";
+		}
+		return "Department with ID: " + id + "does not exists"; 
+		
+	}
 
 }
