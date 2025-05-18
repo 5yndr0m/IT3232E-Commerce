c2a6e00050fae90976b9575f543c5be41cf57020
5yndr0m <sdilanjana21@gmail.com>
2025-05-18 22:09:47 +0530

    feat: link request entities to services

--- AppwithDB/src/main/java/com/vau/app/controller/DepartmentController.java
+++ AppwithDB/src/main/java/com/vau/app/controller/DepartmentController.java
@@ -1,12 +1,15 @@
 package com.vau.app.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +35,31 @@ public class DepartmentController {
 			return new ResponseEntity<>("Department with ID: " + dep_id + " not found", HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(service.getDept(dep_id), HttpStatus.OK);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> addDept(@RequestBody Department dept){
+		if(dept != null) {
+			return new ResponseEntity<String>(service.addDept(dept), HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("No data provided", HttpStatus.BAD_REQUEST);
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> updateDept(@PathVariable("id") String id, @RequestBody Department dept){
+		if(dept != null) {
+			return new ResponseEntity<String>(service.updateDept(id, dept), HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<String>("No data provided", HttpStatus.BAD_REQUEST);
+	}
+	
+	@DeleteMapping("/remove/{id}")
+	public ResponseEntity<String> deleteDept(@PathVariable("id") String id){
+		if(id != null) {
+			return new ResponseEntity<String>(service.deleteDept(id), HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<String>("No data provided", HttpStatus.BAD_REQUEST);
 	}
 }
