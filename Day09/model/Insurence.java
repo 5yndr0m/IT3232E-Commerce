1f9c63e1a6034e2b95f1e89b77e93c88495ba9f0
5yndr0m <sdilanjana21@gmail.com>
2025-05-28 14:30:52 +0530

    feat: add models for day09

---
+++ Day09/model/Insurence.java
@@ -0,0 +1,43 @@
+package com.employee.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Insurence {
+
+//	Attributes
+	@Id
+	private String insID;
+	private String pkgType;
+	private int noOfYears;
+	@OneToOne(mappedBy = "insurence")
+	private Employee employee;
+	public String getInsID() {
+		return insID;
+	}
+	public void setInsID(String insID) {
+		this.insID = insID;
+	}
+	public String getPkgType() {
+		return pkgType;
+	}
+	public void setPkgType(String pkgType) {
+		this.pkgType = pkgType;
+	}
+	public int getNoOfYears() {
+		return noOfYears;
+	}
+	public void setNoOfYears(int noOfYears) {
+		this.noOfYears = noOfYears;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	
+}
