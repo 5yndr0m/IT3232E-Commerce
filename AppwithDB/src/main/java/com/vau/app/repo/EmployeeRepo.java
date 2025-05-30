2a63208c82705be263817e64305216afb8d6285f
5yndr0m <sdilanjana21@gmail.com>
2025-05-31 01:43:09 +0530

    feat: add methods with queries

--- AppwithDB/src/main/java/com/vau/app/repo/EmployeeRepo.java
+++ AppwithDB/src/main/java/com/vau/app/repo/EmployeeRepo.java
@@ -1,9 +1,17 @@
 package com.vau.app.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.vau.app.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, String>{
 
+	@Query("SELECT e FROM Employee e WHERE e.salary BETWEEN %?1% AND %?2%")
+	public List<Employee> findInRange(int min, int max);
+	
+	@Query("SELECT e FROM Employee e WHERE e.age = (SELECT MIN(age) FROM Employee)")
+	public Employee findYoungest();
 }
